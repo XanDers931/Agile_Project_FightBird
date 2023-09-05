51252b34281c1a22b3780355c1be9afe7e29a54e
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2023-09-05 11:12:22 +0200

    visual Shifumi update

--- src/main/Shifumi.java
+++ src/main/Shifumi.java
@@ -24,8 +24,76 @@ public class Shifumi {
         return computerPick;
     }
 
+    private void printPick(String computerPick){
+        //own pick
+        if(actualPick.equals("Paper")){
+            System.out.println("You pick Paper :\n \"\"\"\n" + //
+                    "     _______\n" + //
+                    "---'    ____)____\n" + //
+                    "           ______)\n" + //
+                    "          _______)\n" + //
+                    "         _______)\n" + //
+                    "---.__________)\n" + //
+                    "\"\"\"");
+        }
+        else if(actualPick.equals("Rock")){
+            System.out.println("You pick Rock :");
+            System.out.println("""
+                _______
+            ---'   ____)
+                  (_____)
+                  (_____)
+                  (____)
+            ---.__(___)
+            """);
+        }
+        else if(actualPick.equals("Scissors")){
+            System.out.println("You pick Scissors :\n \"\"\"\n" + //
+                    "    _______\n" + //
+                    "---'   ____)____\n" + //
+                    "          ______)\n" + //
+                    "       __________)\n" + //
+                    "      (____)\n" + //
+                    "---.__(___)\n" + //
+                    "\"\"\")");
+        }
+        //Ennemi pick
+        if(computerPick.equals("Paper")){
+            System.out.println("Your ennemi pick Paper :\n \"\"\"\n" + //
+                    "     _______\n" + //
+                    "---'    ____)____\n" + //
+                    "           ______)\n" + //
+                    "          _______)\n" + //
+                    "         _______)\n" + //
+                    "---.__________)\n" + //
+                    "\"\"\"");
+        }
+        else if(computerPick.equals("Rock")){
+            System.out.println("Your ennemi pick Rock :");
+            System.out.println("""
+                _______
+            ---'   ____)
+                  (_____)
+                  (_____)
+                  (____)
+            ---.__(___)
+            """);
+        }
+        else if(computerPick.equals("Scissors")){
+            System.out.println("Your ennemi pick Scissors :\n \"\"\"\n" + //
+                    "    _______\n" + //
+                    "---'   ____)____\n" + //
+                    "          ______)\n" + //
+                    "       __________)\n" + //
+                    "      (____)\n" + //
+                    "---.__(___)\n" + //
+                    "\"\"\")");
+        }
+    }
+
     public boolean fight(){
         String computerPick = randompick();
+        printPick(computerPick);
         if(computerPick.equals("Rock") && actualPick.equals("Paper")){
             return true;
         }
