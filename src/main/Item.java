55839ddbcd13480d057cc1b6bcde0dbabdfe7383
Paul Cancel <paul.cancel.etu@univ-lille.fr>
2023-09-06 11:42:12 +0200

    Fix Item + Importweapons

--- src/main/Item.java
+++ src/main/Item.java
@@ -50,4 +50,46 @@ public abstract class Item implements Comparable<Item>{
     public int compareTo(Item item){
         return this.type.compareTo(item.type);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + damage;
+        result = prime * result + critical_rate;
+        result = prime * result + accuracy;
+        result = prime * result + rarity;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Item other = (Item) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (type != other.type)
+            return false;
+        if (damage != other.damage)
+            return false;
+        if (critical_rate != other.critical_rate)
+            return false;
+        if (accuracy != other.accuracy)
+            return false;
+        if (rarity != other.rarity)
+            return false;
+        return true;
+    }
+
+    
 }
